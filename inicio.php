ce645c830360ec808a9ba11106bdf60f08426379
josedlopezc <joselopez315896@correo.itm.edu.co>
2025-04-23 16:30:31 -0500

    Create inicio.php

---
+++ inicio.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FitCheck</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img src="images/logo1.png" class="logo">
+        <nav>
+            <a href="inicio.php">Inicio</a>
+            <a href="rutina.php">Rutinas</a>
+            <a href="alimentacion.php">Alimentación</a>
+        </nav>
+    </header>
+    <section id="banner">
+        <div class="contenido-banner">
+            <h3><span>FitCheck: </span>Tu mejor aliado en tu cuidado personal<br> y estilo de vida.</h3>
+        </div>
+    </section>
+    <section id="servicios">
+        <div>
+            <img src="./images/ejercicio.jpg">
+            <h4>Recomendaciones de rutina</h4>
+            <p>¡Descubre la rutina ideal para ti!</p> 
+            <a href="rutina.php" class="boton-saber-mas">Saber más</a>
+        </div>
+        <div>
+            <img src="./images/alimentacion.jpg">
+            <h4>Recomendaciones de alimentación</h4>
+            <p>¡Descubre la alimentación ideal para ti!</p> 
+            <a href="alimentacion.php" class="boton-saber-mas">Saber más</a>
+        </div>
+    </section>
+    <footer>
+        &copy:2025 FitCheck (Todos los derechos reservados)
+    </footer>
+</body>
+</html>
